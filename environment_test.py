053d36306232f1ef495d777909b6098f6f4eb292
Spark <spark-li@users.noreply.github.com>
2019-09-18 16:06:35 -0500

    Environment Set up Instruction
    Environment Set up Instruction
    1. Instructions in README.md
    2. A screenshot at Src\pics\
    3. A simple test python file

---
+++ environment_test.py
@@ -0,0 +1,7 @@
+import numpy, cv2, dlib, imutils
+
+
+print(f'Version of numpy is:', numpy.__version__)
+print(f'Version of OpenCV is:', cv2.__version__)
+print(f'Version of dlib is:', dlib.__version__)
+print(f'Version of imutils is:', imutils.__version__)
