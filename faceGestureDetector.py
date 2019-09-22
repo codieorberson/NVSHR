612d9cacba050fb11b7fa0539a65a073099be04c
jcf1991 <jcf1991@missouristate.edu>
2019-09-22 07:49:48 -0500

    Pulled face/wink detection code out into another file, cleaning up gestureDetector.py so that the high level flow of the program is more readable.

--- faceGestureDetector.py
+++ faceGestureDetector.py
@@ -1,6 +1,7 @@
+import cv2
 from gesture import Gesture
 
-class HandGestureDetector():
+class FaceGestureDetector():
     def __init__(self):
         self.has_made_left_wink = False
         self.has_made_right_wink = False
@@ -8,8 +9,8 @@ class HandGestureDetector():
         self.left_wink_callback = None
         self.right_wink_callback = None
 
-        face_cascade = cv2.CascadeClassifier('face.xml')
-        eye_cascade = cv2.CascadeClassifier('eye.xml')
+        self.face_cascade = cv2.CascadeClassifier('face.xml')
+        self.eye_cascade = cv2.CascadeClassifier('eye.xml')
 
 #        self.left_wink = Gesture("eye.xml")
 #        self.right_wink = Gesture("eye.xml")
@@ -22,9 +23,39 @@ class HandGestureDetector():
     def on_right_wink(self, callback):
         self.right_wink_callback = callback
 
-    def detect(self, frame):
-        self.fist.detect(frame)
-        self.palm.detect(frame)
+    def detect(self, frame, gray, cap):
+
+        faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+        center_pixel = cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2
+        center_face = None
+
+        for (x,y,w,h) in faces:
+            center_offset = center_pixel - (x + (w / 2))
+            if not center_face or abs(center_offset) - abs(center_face[4]) > 0:
+                center_face = (x, y, w, h, center_offset)
+
+        if center_face:
+            x = center_face[0]
+            y = center_face[1]
+            w = center_face[2]
+            h = center_face[3]
+            center_of_face = x + (w / 2)
+
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            eyes = self.eye_cascade.detectMultiScale(roi_gray)
+
+            if len(eyes) == 1:
+                if (x + eyes[0][0] + (eyes[0][2] / 2)) < center_of_face:
+                    self.has_made_left_wink = True
+                else:
+                    self.has_made_right_wink = True
+
+            for (ex,ey,ew,eh) in eyes:
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
         
     def cycle(self):
 
--- gestureDetector.py
+++ gestureDetector.py
@@ -1,19 +1,16 @@
 import cv2
 from timer import Timer
 from gesture import Gesture
+#I really don't like the following two module names, because I feel like they
+#imply that they extend THIS module while the reverse is actually true.
 from handGestureDetector import HandGestureDetector
+from faceGestureDetector import FaceGestureDetector
 
 class GestureDetector():
     def __init__(self, time_increment):
         self.time_increment = time_increment
-
-        self.has_made_left_wink = False
-        self.has_made_right_wink = False
-
-        self.left_wink_callback = None
-        self.right_wink_callback = None
-
         self.hand_gesture_detector = HandGestureDetector()
+        self.face_gesture_detector = FaceGestureDetector()
 
     def on_fist(self, callback):
         self.hand_gesture_detector.on_fist(callback)
@@ -22,24 +19,17 @@ class GestureDetector():
         self.hand_gesture_detector.on_palm(callback)
 
     def on_left_wink(self, callback):
-        self.left_wink_callback = callback
+        self.face_gesture_detector.on_left_wink(callback)
 
     def on_right_wink(self, callback):
-        self.right_wink_callback = callback
+        self.face_gesture_detector.on_right_wink(callback)
 
     def __on_tick__(self):
         #The next line is just for debugging, we need to remove it eventually.
         print("tick")
 
         self.hand_gesture_detector.cycle()
-
-        if self.has_made_left_wink and self.left_wink_callback:
-            self.left_wink_callback()
-            self.has_made_left_wink = False
-
-        if self.has_made_right_wink and self.right_wink_callback:
-            self.right_wink_callback()
-            self.has_made_right_wink = False
+        self.face_gesture_detector.cycle()
 
     def start(self):
         timer = Timer(self.time_increment)
@@ -47,46 +37,12 @@ class GestureDetector():
 
         cap = cv2.VideoCapture(0)
 
-        face_cascade = cv2.CascadeClassifier('face.xml')
-        eye_cascade = cv2.CascadeClassifier('eye.xml')
-
         while(True):
             ret, frame = cap.read()
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             self.hand_gesture_detector.detect(frame)
-
-            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-            center_pixel = cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2
-            center_face = None
-
-            for (x,y,w,h) in faces:
-                center_offset = center_pixel - (x + (w / 2))
-                if not center_face or abs(center_offset) - abs(center_face[4]) > 0:
-                    center_face = (x, y, w, h, center_offset)
-
-            if center_face:
-                x = center_face[0]
-                y = center_face[1]
-                w = center_face[2]
-                h = center_face[3]
-                center_of_face = x + (w / 2)
-
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-                roi_gray = gray[y:y+h, x:x+w]
-                roi_color = frame[y:y+h, x:x+w]
-                eyes = eye_cascade.detectMultiScale(roi_gray)
-
-                if len(eyes) == 1:
-                    if (x + eyes[0][0] + (eyes[0][2] / 2)) < center_of_face:
-                        self.has_made_left_wink = True
-                    else:
-                        self.has_made_right_wink = True
-
-                for (ex,ey,ew,eh) in eyes:
-                    roi_gray = gray[y:y+h, x:x+w]
-                    roi_color = frame[y:y+h, x:x+w]
-                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            self.face_gesture_detector.detect(frame, gray, cap)
 
             timer.check_time()
             cv2.imshow('insaneInTheFrame', cv2.flip(frame, 1))
