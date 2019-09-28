7063ee6cc26c073cd81252924aebdf0104d46721
jcf1991 <jcf1991@missouristate.edu>
2019-09-28 18:38:08 -0500

    Added a class for managing a bunch of processes.

---
+++ processManager.py
@@ -0,0 +1,22 @@
+from multiprocessing import Process
+
+class ProcessManager():
+    def __init__(self):
+        self.processes = []
+
+    def add_process(self, callback, arguments = None):
+        if arguments:
+            if not isinstance(arguments, tuple):
+                arguments = (arguments, )
+            process = Process(target = callback, args = arguments)
+        else:
+            process = Process(target = callback)
+        self.processes.append(process)
+        process.start()
+
+    def on_done(self, callback = None):
+        for process in self.processes:
+            process.join()
+        self.processes = []
+        if callback:
+            callback()
