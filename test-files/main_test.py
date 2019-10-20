80eda3b77934f60fe573e732462644cce9f887bc
jcf1991 <jcf1991@missouristate.edu>
2019-10-20 18:07:18 -0500

    After reviewing commits on various branches, I uncommented Danielle's recently added blink tests (I was previously unsure if these should be merged or not, as indicated by the acoompanying now deleted comments).

--- test-files/main_test.py
+++ test-files/main_test.py
@@ -2,10 +2,7 @@ from subprocess import call
 
 
 if __name__ == '__main__':
-#It's not clear to me if the next line should be merged or not,
-#the tests still don't run on my machine so I'm leaving it up to
-#somebody else to uncomment this or delete it:
-#    call(["python", "blinkDetectorTest.py"])
+    call(["python", "blinkDetectorTest.py"])
     call(["python", "gestureTest.py"])
     call(["python", "gestureLexerTest.py"])
     call(["python", "gestureParserTest.py"])
