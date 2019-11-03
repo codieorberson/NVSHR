b6252283fcc61c8de7829ec9aa065b79ac946e17
Landan Ginther <landanginther25@gmail.com>
2019-11-03 12:27:30 -0600

    Minor change.

--- gestureParser.py
+++ gestureParser.py
@@ -9,14 +9,14 @@ class GestureParser():
     def add_pattern(self, gestures, event):
         self.gesture_pattern_map["".join(gestures)] = event
 
-    #Takes in a list of lists of gestures and matches them to any patterns under add_pattern
-    #Then sends confirm or failure noise, and logs the sequence in logger.
+    # Takes in a list of lists of gestures and matches them to any patterns under add_pattern
     def parse_pattern(self, gesture_sequence, now):
         joined_gesture_sequence = "".join(gesture_sequence)
         was_recognised = bool(
             joined_gesture_sequence in self.gesture_pattern_map)
         self.logger.log_gesture_sequence(gesture_sequence, now, was_recognised)
 
+        # Then sends confirm or failure noise, and logs the sequence in logger.
         if was_recognised:
             self.gesture_pattern_map[joined_gesture_sequence]()
             Sound.success()
