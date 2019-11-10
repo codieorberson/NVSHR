9f8928a3b7dcef293aac9f8320608354a2c752f7
Codie Orberson <31399048+codieorberson@users.noreply.github.com>
2019-11-10 14:48:57 -0600

    Running the TpLink class in our system

--- gestureParser.py
+++ gestureParser.py
@@ -1,10 +1,12 @@
 from sound import Sound
+from TPLink import TPLinkDevice
 
 class GestureParser():
     def __init__(self, logger, database_manager):
         self.logger = logger
         self.database_manager = database_manager
         self.gesture_pattern_map = {}
+        self.Tp_Link_Devices = TPLinkDevice()
 
     def add_pattern(self, gestures, event):
         #        self.
@@ -19,6 +21,7 @@ class GestureParser():
         self.logger.log_gesture_sequence(gesture_sequence, now, was_recognised)
 
         if was_recognised:
+            self.Tp_Link_Devices.turn_on_off(gesture_sequence)
             self.gesture_pattern_map[joined_gesture_sequence]()
             Sound.success()
         else:
