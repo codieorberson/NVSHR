b579b0502f224b62cf5ea3fd6b8d01368b830e2f
jcf1991 <jcf1991@missouristate.edu>
2019-11-11 11:48:15 -0600

    Removed optional callback from SubprocessExecutor.execute -- I rethought the need for it, and did what I was trying to do another way last commit.

--- subprocessExecutor.py
+++ subprocessExecutor.py
@@ -2,22 +2,17 @@ from subprocess import Popen
 
 class SubprocessExecutor():
     def __init__(self):
-        self.subprocess_maps = []
+        self.subprocesses = []
 
-    def execute(self, file_name, argument, callback = None):
+    def execute(self, file_name, argument):
         self.__clean_up_subprocesses__()
-        self.subprocess_maps.append({
-                'process' : Popen(['python3', file_name, argument]),
-                'callback' : callback
-                })
+        self.subprocesses.append(Popen(['python3', file_name, argument]))
 
     def __clean_up_subprocesses__(self):
-        running_subprocess_maps = []
-        for subprocess_map in self.subprocess_maps:
-            if subprocess_map['process'].poll() == None:
-                running_subprocess_maps.append(subprocess_map)
+        running_subprocesses = []
+        for subprocess in self.subprocesses:
+            if subprocess.poll() == None:
+                running_subprocesses.append(subprocess)
             else:
-                subprocess_map['process'].terminate()
-                if subprocess_map['callback']:
-                    subprocess_map['callback']()
-        self.subprocess_maps = running_subprocess_maps
+                subprocess.terminate()
+        self.subprocesses = running_subprocesses
