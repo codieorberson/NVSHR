3997f78d008f7c3e9b01ec403c6b258c2b0fff11
jcf1991 <jcf1991@missouristate.edu>
2019-11-12 09:08:20 -0600

    Properly added new folders to system path in main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #!/usr/local/bin/python3
 import sys
-sys.path.insert(1, './modules/')
-sys.path.insert(2, './modules/gui/')
-sys.path.insert(3, './static_data/')
-sys.path.insert(4, './dynamic_data/')
+sys.path.insert(1, './static_data/')
+sys.path.insert(2, './dynamic_data/')
+sys.path.insert(3, './modules/')
+sys.path.insert(4, './modules/gesture/')
+sys.path.insert(5, './modules/smart_home/')
+sys.path.insert(6, './modules/gui/')
 
 from nonVerbalSmartHomeRecognitionSystem import NonVerbalSmartHomeRecognitionSystem
 
