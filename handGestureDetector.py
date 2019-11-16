d3880ca9fea96cd72b97275824562a4d8433c03c
Codie Orberson <31399048+codieorberson@users.noreply.github.com>
2019-11-16 15:39:19 -0600

    Taking out unused lines of code as well as adding detection for the palm gesture

--- handGestureDetector.py
+++ handGestureDetector.py
@@ -1,18 +1,19 @@
 from processManager import ProcessManager
 from gesture import Gesture
-
+from frameContrast import FrameContrast
 
 class HandGestureDetector():
     def __init__(self):
         self.process_manager = ProcessManager()
+        self.palm_frame = FrameContrast()
         self.fist = Gesture("fist.xml")
         self.palm = Gesture("palm.xml")
-        self.fist.set_debug_color((0, 0, 255))
-        self.palm.set_debug_color((0, 255, 255))
-
-    def detect(self, frame, has_made_fist, has_made_palm):
+        
+    def detect(self, frame, palm_low_contrast_frame, palm_high_contrast_frame, has_made_fist, has_made_palm):
+        self.new_palm_frame = self.palm_frame.changing_palm_frame(frame, palm_low_contrast_frame, palm_high_contrast_frame)
         self.process_manager.add_process(
                 self.fist.detect, (frame, has_made_fist))
         self.process_manager.add_process(
-                self.palm.detect, (frame, has_made_palm))
+                self.palm.detect, (self.new_palm_frame, has_made_palm))
         self.process_manager.on_done()
+
