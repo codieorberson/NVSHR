3e60e4920b80b390aa1e2e8ed6909dd62dc4c173
jcf1991 <jcf1991@missouristate.edu>
2019-11-16 02:21:12 -0600

    Radically changed the initialization procedure of GuiManager, separating that one big init call with a gabillion arguments into a bunch of setter methods. I know, this means we have a bigger interface to test. It also means that we don't have an init statement with a gabillion arguments, so that's preferable. Still need to change the GuiTab.

--- guiManager.py
+++ guiManager.py
@@ -2,33 +2,60 @@
 from guiWindow import GuiWindow
 
 class GuiManager():
-    def __init__(self, cap, on_ear_change,
-                 initial_ear, on_low_contrast, initial_low_contrast,
-                 on_high_contrast, initial_high_contrast,
-                 on_min_time_inc, initial_min_time_inc,
-                 on_max_time_inc, initial_max_time_inc,
-                 gesture_detected, is_admin, database_manager):
+    def __init__(self, cap, settings_manager, is_admin):
+        self.cap = cap
+        self.settings_manager = settings_manager
         self.gui = GuiWindow()
         self.gui.title("Non-Verbal Smart Home Recognition System")
-        self.debug_tab = self.gui.set_cap_and_get_debug_tab(cap, on_ear_change, initial_ear,
-                                                            on_low_contrast, initial_low_contrast,
-                                                            on_high_contrast, initial_high_contrast,
-                                                            on_min_time_inc, initial_min_time_inc,
-                                                            on_max_time_inc, initial_max_time_inc,
-                                                            gesture_detected, database_manager)
-        self.fps_tab = self.gui.get_fps_tab()
-        self.blink_label = self.gui.get_blink_label()
-        self.fist_label = self.gui.get_fist_label()
-        self.palm_label = self.gui.get_palm_label()
 
         if is_admin == False:
             self.gui.withdraw()
+
+    def set_initial_ear(self, initial_value):
+        self.gui.set_initial_ear(initial_value)
+
+    def set_initial_low_contrast(self, initial_value):
+        self.gui.set_initial_low_contrast(initial_value)
+
+    def set_initial_high_contrast(self, initial_value):
+        self.gui.set_initial_high_contrast(initial_value)
+
+    def set_initial_minimum_time_increment(self, initial_value):
+        self.gui.set_initial_minimum_time_increment(initial_value)
+
+    def set_initial_maximum_time_increment(self, initial_value):
+        self.gui.set_initial_maximum_time_increment(initial_value)
+
+    def on_ear_change(self, callback):
+        self.gui.on_ear_change(callback)
+
+    def on_low_contrast_change(self, callback):
+        self.gui.on_low_contrast_change(callback)
+
+    def on_high_contrast_change(self, callback):
+        self.gui.on_high_contrast_change(callback)
+
+    def on_minimum_time_increment_change(self, callback):
+        self.gui.on_minimum_time_increment_change(callback)
+
+    def on_maximum_time_increment_change(self, callback):
+        self.gui.on_maximum_time_increment_change(callback)
+
+    def on_new_command(self, callback):
+        self.gui.on_new_command(callback)
 
     def __loop__(self):
         self.loop_callback()
         self.gui.after(1, self.__loop__)
 
     def start(self, loop_callback, close_callback):
+        self.gui.set_cap(self.cap, self.settings_manager)
+        self.debug_tab = self.gui.get_debug_tab()
+        self.fps_tab = self.gui.get_fps_tab()
+        self.blink_label = self.gui.get_blink_label()
+        self.fist_label = self.gui.get_fist_label()
+        self.palm_label = self.gui.get_palm_label()
+
         self.loop_callback = loop_callback
         self.close_callback = close_callback
         self.gui.protocol("WM_DELETE_WINDOW", close_callback)
--- guiWindow.py
+++ guiWindow.py
@@ -9,22 +9,50 @@ class GuiWindow(Tk):
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
 
-    def set_cap_and_get_debug_tab(self, cap, on_ear_change, initial_ear,
-                                  on_low_contrast, initial_low_contrast,
-                                  on_high_contrast, initial_high_contrast,
-                                  on_min_time_inc, initial_min_time_inc,
-                                  on_max_time_inc, initial_max_time_inc,
-                                  gesture_detected, settings_manager):
+    def set_initial_ear(self, initial_value):
+        self.initial_ear = initial_value
+
+    def set_initial_low_contrast(self, initial_value):
+        self.initial_low_contrast = initial_value
+
+    def set_initial_high_contrast(self, initial_value):
+        self.initial_high_contrast = initial_value
+
+    def set_initial_minimum_time_increment(self, initial_value):
+        self.initial_minimum_time_increment = initial_value
+
+    def set_initial_maximum_time_increment(self, initial_value):
+        self.initial_maximum_time_increment = initial_value
+
+    def on_ear_change(self, callback):
+        self.on_ear_change = callback
+
+    def on_low_contrast_change(self, callback):
+        self.on_low_contrast_change = callback
+
+    def on_high_contrast_change(self, callback):
+        self.on_high_contrast_change = callback
+
+    def on_minimum_time_increment_change(self, callback):
+        self.on_minimum_time_increment_change = callback
+
+    def on_maximum_time_increment_change(self, callback):
+        self.on_maximum_time_increment_change = callback
+
+    def on_new_command(self, callback):
+        self.on_new_command_change = callback
+
+    def set_cap(self, cap, settings_manager):
         self.notebook = ttk.Notebook(width=1000, height=800)
-        self.debug_tab = self.add_content(gui_data, cap, on_ear_change, initial_ear, on_low_contrast,
-                                          initial_low_contrast,
-                                          on_high_contrast, initial_high_contrast,
-                                          on_min_time_inc, initial_min_time_inc,
-                                          on_max_time_inc, initial_max_time_inc,
-                                          gesture_detected, settings_manager)
+        self.debug_tab = self.add_content(gui_data, cap, 
+                self.on_ear_change, self.initial_ear, 
+                self.on_low_contrast_change, self.initial_low_contrast,
+                self.on_high_contrast_change, self.initial_high_contrast,
+                self.on_minimum_time_increment_change, self.initial_minimum_time_increment,
+                self.on_maximum_time_increment_change, self.initial_maximum_time_increment,
+                self.on_new_command_change, settings_manager)
 
         self.notebook.grid(row=0)
-        return self.debug_tab
 
     def add_content(self, body, cap, on_ear_change, initial_ear,
                     on_low_contrast, initial_low_contrast,
@@ -32,14 +60,20 @@ class GuiWindow(Tk):
                     on_min_time_inc, initial_min_time_inc,
                     on_max_time_inc, initial_max_time_inc,
                     gesture_detected, settings_manager):
+
         for i in range(len(list(body.keys()))):
             page_configuration = body[list(body.keys())[i]]
-            tab = GuiTab(self.notebook, self, cap, on_ear_change, initial_ear, on_low_contrast, initial_low_contrast,
-                       on_high_contrast, initial_high_contrast,
-                       on_min_time_inc, initial_min_time_inc,
-                       on_max_time_inc, initial_max_time_inc, gesture_detected, page_configuration["elements"],
-                       settings_manager)
+            tab = GuiTab(self.notebook, self, cap, 
+                    on_ear_change, initial_ear, 
+                    on_low_contrast, initial_low_contrast,
+                    on_high_contrast, initial_high_contrast,
+                    on_min_time_inc, initial_min_time_inc,
+                    on_max_time_inc, initial_max_time_inc, 
+                    gesture_detected, page_configuration["elements"],
+                    settings_manager)
+
             self.notebook.add(tab, text=page_configuration["title"])
+
             if tab.is_debug:
                 debug_tab = tab
             if tab.is_fps:
@@ -52,6 +86,9 @@ class GuiWindow(Tk):
                 self.palm_label = tab
 
         return debug_tab
+
+    def get_debug_tab(self):
+        return self.debug_tab
 
     def get_fps_tab(self):
         return self.fps_tab
