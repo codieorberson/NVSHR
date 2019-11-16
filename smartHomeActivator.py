7871315ea8d820621a49cf0c5e192fff26769767
Codie Orberson <31399048+codieorberson@users.noreply.github.com>
2019-11-15 20:33:46 -0600

    Added an exception to test out program when not connected to smartplug

--- smartHomeActivator.py
+++ smartHomeActivator.py
@@ -1,5 +1,5 @@
 from TPLink import TPLinkDevice
-from sound import Sound
+from sound import Sound 
 
 class SmartHomeActivator():
     def __init__(self, database_manager):
@@ -9,15 +9,17 @@ class SmartHomeActivator():
 
     def activate(self, gesture_sequence, was_recognized):
         if was_recognized:
-            self.turn_On_Off_TpLink_Device(gesture_sequence)
-            self.gesture_pattern_map[joined_gesture_sequence]()
-            Sound.success()
+            try:
+                self.turn_on_off_TpLink_Device(gesture_sequence)
+                Sound.success()
+            except:
+                print("Unable to connect command to requested smart home device")
         else:
             Sound.failure()
 
     #Iterating through the command dictionary and performing smart home action linked
     #with the given gesture sequence
-    def turn_On_Off_TpLink_Device(self, gesture_sequence):
+    def turn_on_off_TpLink_Device(self, gesture_sequence):
         index = 0
         while(index < len(self.commands)):
             for key in self.commands[index]:
