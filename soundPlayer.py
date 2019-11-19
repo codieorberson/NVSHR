b3dc58ea5cdae4914af691135841e63b41100ae6
jcf1991 <jcf1991@missouristate.edu>
2019-11-18 18:31:40 -0600

    Refactor SoundPlayer using Spark's better code.

--- soundPlayer.py
+++ soundPlayer.py
@@ -1,13 +1,7 @@
-from subprocessExecutor import SubprocessExecutor
-from sound import Sound
-from platform import system
+# Sound Effects by Eric Matyas,  www.soundimage.org
+from playsound import playsound
 
 class SoundPlayer():
-    def __init__(self):
-        self.subprocess_executor = SubprocessExecutor()
-        self.sound = Sound()
-        self.is_linux = system() == 'Linux'
-
     def play_success_sound(self):
         self.__play_file__('Success.wav')
 
@@ -15,7 +9,4 @@ class SoundPlayer():
         self.__play_file__('Failure.wav')
 
     def __play_file__(self, file_name):
-        if self.is_linux:
-            self.subprocess_executor.execute('./play_file.py', file_name)
-        else:
-            self.sound(file_name)
+        playsound(file_name, False)
