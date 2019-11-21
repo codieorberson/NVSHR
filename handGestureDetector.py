67895068d60762b05bdcffff40c0826f3929e7e6
Codie Orberson <31399048+codieorberson@users.noreply.github.com>
2019-11-20 18:48:36 -0600

    Made multithreaded perimeter apply properly to left fist and palm

--- handGestureDetector.py
+++ handGestureDetector.py
@@ -1,18 +1,16 @@
 from processManager import ProcessManager
 from gesture import Gesture
-from frameContrast import FrameContrast
 
 class HandGestureDetector():
     def __init__(self):
         self.process_manager = ProcessManager()
-        self.palm_frame = FrameContrast()
         self.fist = Gesture("fist.xml")
         self.palm = Gesture("palm.xml")
         
     def detect(self, frame, flipped_frame, has_made_fist, has_made_palm):
         self.process_manager.add_process(
-                self.fist.detect, (frame, flipped_frame, has_made_fist))
+                self.fist.detect_fist, (frame, flipped_frame, has_made_fist))
         self.process_manager.add_process(
-                self.palm.detect, (frame, flipped_frame, has_made_palm))
+                self.palm.detect_palm, (frame, flipped_frame, has_made_palm))
         self.process_manager.on_done()
 
