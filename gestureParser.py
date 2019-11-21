35661bd2a4d0f9038abb986ef43f5fc513b623e8
jcf1991 <jcf1991@missouristate.edu>
2019-11-21 09:57:30 -0600

    Added detection of valid subsets of gesture sequences to gestureParser.py.

--- gestureParser.py
+++ gestureParser.py
@@ -4,14 +4,21 @@ class GestureParser():
         self.gesture_sequence_events = []
 
     def add_pattern(self, gestures):
-        self.gesture_pattern_map["".join(gestures)] = True
+        self.gesture_pattern_map["-".join(gestures)] = True
 
     def on_gesture_sequence(self, event):
         self.gesture_sequence_events.append(event)
 
     def parse_pattern(self, gesture_sequence, now):
-        joined_gesture_sequence = "".join(gesture_sequence)
+        joined_gesture_sequence = "-".join(gesture_sequence)
         was_recognised = joined_gesture_sequence in self.gesture_pattern_map
+
+        if not was_recognised:
+            for valid_gesture_sequence in self.gesture_pattern_map.keys():
+                if  valid_gesture_sequence in joined_gesture_sequence:
+                    gesture_sequence = valid_gesture_sequence.split("-")
+                    was_recognised = True
+                    break
 
         for event in self.gesture_sequence_events:
             event(gesture_sequence, now, was_recognised)
