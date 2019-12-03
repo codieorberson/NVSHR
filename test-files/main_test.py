5be9e328f3c5cd02514e3087d7fd79ff66261050
Landan Ginther <landanginther25@gmail.com>
2019-12-02 21:26:01 -0600

    Removing the test files directory.

--- test-files/main_test.py
+++
@@ -1,9 +0,0 @@
-from subprocess import call
-
-if __name__ == '__main__':
-    call(["python", "gestureDetectorTest.py"])
-    call(["python", "blinkDetectorTest.py"])
-    call(["python", "blinkTest.py"])
-    call(["python", "gestureLexerTest.py"])
-    call(["python", "gestureParserTest.py"])
-    call(["python", "gestureTest.py"])
